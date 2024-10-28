8a4805461e2a14d3068ba78dcdb4c7d723491415
Amish Ranjan <amishranjan75@gmail.com>
2024-10-28 19:00:54 +0530

    end of pagination and favorites

--- client/src/App.js
+++ client/src/App.js
@@ -28,11 +28,20 @@ export default App;
 
 import { ChakraProvider } from '@chakra-ui/react'
 import ProductsScreen from './screens/ProductsScreen';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
     <ChakraProvider>
-     <ProductsScreen/>
+      <Router>
+        <Header />
+        <main>
+          <Routes>
+            <Route path='/' element = {<ProductsScreen />} />
+          </Routes>
+        </main>
+      </Router>
     </ChakraProvider>
   );
 }
